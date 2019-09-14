f466ca360a61ad29000dbb029f765354751ca3a5
Felix Khoja <o.khoja93@gmail.com>
2019-09-14 18:14:04 -0400

    Set Version 1 (Basic) complete

--- Set/Game.swift
+++ Set/Game.swift
@@ -11,6 +11,7 @@ import Foundation
 struct Game {
     
     private(set) var score = 0
+    private(set) var setCount = 0
     private(set) var setCardDeck = [Card]()
     
     private(set) var cardsOnTheTable = [Card]()
@@ -19,45 +20,42 @@ struct Game {
     private(set) var selectedCardsAreASet = Bool()
     
     
-    mutating func chooseCard(at index: Int){
+    
+    mutating func selectCard(at index: Int) {
+        selectedCardIndices.append(index)
+        selectedCards.append(cardsOnTheTable[index])
+        print([selectedCards.description])
+    }
+    
+    mutating func deSelectCard(at index: Int) {
+        selectedCardIndices.remove(at: selectedCardIndices.firstIndex(of: index)!)
+        selectedCards.remove(at: selectedCards.firstIndex(of: cardsOnTheTable[index])!)
+        print([selectedCards.description])
         
-        //FIX Not registering duplicates
-        
-        if selectedCardIndices.count < 3 {
-            
-            if selectedCardIndices.contains(index) {
-                selectedCardIndices.remove(at: selectedCardIndices.firstIndex(of: index)!)
-                selectedCards.remove(at: selectedCards.firstIndex(of: cardsOnTheTable[index])!)
-                print([selectedCards.description])
-                return
-            }  else {
-                selectedCardIndices.append(index)
-                selectedCards.append(cardsOnTheTable[index])
-                print([selectedCards.description])
+    }
+    
+    mutating func resetCards() {
+        if isASet(with: selectedCards) {
+            print("That's a set! =)")
+            selectedCardsAreASet = true
+            for index in selectedCardIndices {
+                cardsOnTheTable.remove(at: index)
+                replace(at: index)
             }
-        }
-        
-        if selectedCardIndices.count == 3 {
-            if isASet(with: selectedCards) {
-                print("That's a set! =)")
-                selectedCardsAreASet = true
-                for index in selectedCardIndices {
-                    cardsOnTheTable.remove(at: index)
-                }
-                selectedCardIndices.removeAll()
-                selectedCards.removeAll()
-                draw()
-                score += 5
-                return
-            } else {
-                selectedCardIndices.removeAll()
-                selectedCards.removeAll()
-                score -= 1
-                print("That's not a set! =(")
-                selectedCardsAreASet = false
-            }
+            selectedCardIndices.removeAll()
+            selectedCards.removeAll()
+            score += 10
+            setCount += 1
+            return
+        } else {
+            selectedCardIndices.removeAll()
+            selectedCards.removeAll()
+            score -= 5
+            print("That's not a set! =(")
+            selectedCardsAreASet = false
         }
     }
+    
     
     func isASet(with selectedCards: [Card]) -> Bool {
         let color = Set(selectedCards.map{$0.cardColor}).count
@@ -71,8 +69,17 @@ struct Game {
     mutating func draw() {
         if setCardDeck.count > 0 {
             for _ in 1...3 {
-                 cardsOnTheTable += [setCardDeck.remove(at: setCardDeck.randomIndex)]
+                cardsOnTheTable += [setCardDeck.remove(at: setCardDeck.randomIndex)]
             }
+            print("Drawing")
+            return
+        }
+    }
+    
+    mutating func replace(at index: Int) {
+        if setCardDeck.count > 0 {
+            cardsOnTheTable.insert(setCardDeck.remove(at: setCardDeck.randomIndex), at: index)
+            print("Replacing")
         }
     }
     
@@ -84,7 +91,7 @@ struct Game {
     
     // MARK: Initialize Game
     init() {
-        for _ in 1...83 {
+        for _ in 1...81 {
             let color = Card.CardColor.allCases.randomElement()!
             let number = Card.CardNumber.allCases.randomElement()!
             let fill = Card.CardFill.allCases.randomElement()!
@@ -100,3 +107,48 @@ extension Array {
         return Int(arc4random_uniform(UInt32(count - 1)))
     }
 }
+
+
+
+
+
+
+//    mutating func chooseCard(at index: Int){
+//
+//        //FIX Not registering duplicates
+//
+//        if selectedCardIndices.count < 3 {
+//
+//            if selectedCardIndices.contains(index) {
+//                selectedCardIndices.remove(at: selectedCardIndices.firstIndex(of: index)!)
+//                selectedCards.remove(at: selectedCards.firstIndex(of: cardsOnTheTable[index])!)
+//                print([selectedCards.description])
+//                return
+//            }  else {
+//                selectedCardIndices.append(index)
+//                selectedCards.append(cardsOnTheTable[index])
+//                print([selectedCards.description])
+//            }
+//        }
+//
+//        if selectedCardIndices.count == 3 {
+//            if isASet(with: selectedCards) {
+//                print("That's a set! =)")
+//                selectedCardsAreASet = true
+//                for index in selectedCardIndices {
+//                    cardsOnTheTable.remove(at: index)
+//                }
+//                selectedCardIndices.removeAll()
+//                selectedCards.removeAll()
+//                draw()
+//                score += 5
+//                return
+//            } else {
+//                selectedCardIndices.removeAll()
+//                selectedCards.removeAll()
+//                score -= 1
+//                print("That's not a set! =(")
+//                selectedCardsAreASet = false
+//            }
+//        }
+//    }
--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -11,66 +11,82 @@ import UIKit
 class ViewController: UIViewController {
     
     private lazy var game = Game()
-    private var selectedButtons = [UIButton]()
     @IBOutlet var setCardButtons: [UIButton]!
-    private var activeCardButtons = 11
-    @IBOutlet weak var alertLabel: UILabel!
+    private var selectedButtons = [UIButton]()
+    private var activeCardButtons = 12
     
-    @IBAction func dealThreeButton(_ sender: UIButton) {
-        game.draw()
-        addGameCards()
+    @IBOutlet weak var alertLabel: UILabel!
+    @IBOutlet weak var deckCountLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func newGameButtonPressed(_ sender: UIButton) {
+        setUpGame()
+    }
+    
+    @IBAction func dealThreeButtonPressed(_ sender: UIButton) {
+        if game.setCardDeck.count > 0 && game.cardsOnTheTable.count < 24 {
+            activeCardButtons += 3
+            game.draw()
+            updateView()
+        } else {
+            alertLabel.text = ("No more cards to Draw.")
+        }
     }
     
     @IBAction func cardButtonPressed(_ sender: UIButton) {
         if let cardIndex = setCardButtons.firstIndex(of: sender) {
-            game.chooseCard(at: cardIndex)
-            chooseButton(at: sender)
+            chooseButton(at: sender, index: cardIndex)
             updateView()
         }
     }
     
-    private func chooseButton(at card: UIButton) {
-        
-        alertLabel.text = nil
+    private func chooseButton(at card: UIButton, index: Int) {
         
         func resetCards() {
+            game.resetCards()
+            if game.setCardDeck.count == 0 {
+                if game.selectedCardsAreASet {
+                    activeCardButtons -= 3
+                    for index in activeCardButtons ... activeCardButtons + 3 {
+                        setCardButtons[index].isHidden = true
+                    }
+                }
+            }
             setCardButtons.forEach() { $0.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) }
             selectedButtons.removeAll()
+            
         }
         
         func deselectCard() {
             card.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
             card.layer.borderWidth = 3.0
             selectedButtons.remove(at: selectedButtons.firstIndex(of: card)!)
+            game.deSelectCard(at: index)
         }
         
         func selectCard(){
             card.layer.borderColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
             card.layer.borderWidth = 3.0
+            game.selectCard(at: index)
+            alertLabel.text = "Sets Found: \(game.setCount)"
         }
         
-        if selectedButtons.count < 3 {
-            
-            if selectedButtons.contains(card) {
-                deselectCard()
-                return
+        if selectedButtons.contains(card) {
+            deselectCard()
+            return
+        } else {
+            selectedButtons.append(card)
+            selectCard()
+        }
+
+        if selectedButtons.count == 3 {
+            resetCards()
+            if game.selectedCardsAreASet {
+                alertLabel.text = "That's a set! =)"
             } else {
-                selectedButtons.append(card)
-                selectCard()
-                if game.selectedCardsAreASet {
-                    resetCards()
-                    alertLabel.text = "That's a set! =)"
-                    return
-                }
+                alertLabel.text = "Not a set! =("
             }
         }
-        
-        if selectedButtons.count == 3 {
-            alertLabel.text = "Not a set! =("
-            resetCards()
-            // updateScore()
-        }
-        
     }
     
     
@@ -79,27 +95,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpGame()
+    }
+    
+    private func setUpGame() {
+        game = Game()
+        activeCardButtons = 11
+        for button in setCardButtons {
+            button.isHidden = true
+        }
         updateView()
     }
     
-    //    private func setUpGame() {
-    //    }
-    
-    private func addGameCards(){
-        if activeCardButtons < 23 {
-            activeCardButtons += 3
-            updateView()
-        }
-    }
-    
     private func updateView() {
-        for index in 0...activeCardButtons{
+        
+        for index in 0...(activeCardButtons){
             let button = setCardButtons[index]
-            button.isHidden = false
             let card = game.cardsOnTheTable[index]
             button.setAttributedTitle(setCardTitle(with: card), for: .normal)
+            button.isHidden = false
         }
+        print("Cards on the table: \(game.cardsOnTheTable.count)")
         
+        scoreLabel.text = "Score: \(game.score)"
+        deckCountLabel.text = "Deck Count: \(game.setCardDeck.count)"
     }
     
     private func setCardTitle(with card: Card) -> NSAttributedString {
@@ -128,3 +147,139 @@ struct ModelToView {
     static var alpha: [Card.CardFill: CGFloat] = [.solid: 1.0, .outlined: 0.40, .striped: 0.15]
     static var strokeWidth: [Card.CardFill: CGFloat] = [.solid: -5, .outlined: 5, .striped: -5]
 }
+
+
+//class ViewController: UIViewController {
+//
+//    private lazy var game = Game()
+//    @IBOutlet var setCardButtons: [UIButton]!
+//    private var selectedButtons = [UIButton]()
+//    private var activeCardButtons = 11
+//
+//    @IBOutlet weak var alertLabel: UILabel!
+//    @IBOutlet weak var deckCountLabel: UILabel!
+//    @IBOutlet weak var scoreLabel: UILabel!
+//
+//    @IBAction func newGameButtonPressed(_ sender: UIButton) {
+//        setUpGame()
+//    }
+//
+//    @IBAction func dealThreeButtonPressed(_ sender: UIButton) {
+//        if game.cardsOnTheTable.count < 24 {
+//            game.draw()
+//            addGameCards()
+//            updateView()
+//        }
+//    }
+//
+//    @IBAction func cardButtonPressed(_ sender: UIButton) {
+//        if let cardIndex = setCardButtons.firstIndex(of: sender) {
+//            chooseButton(at: sender, index: cardIndex)
+//            updateView()
+//        }
+//    }
+//
+//    private func chooseButton(at card: UIButton, index: Int) {
+//
+//        func resetCards() {
+//            setCardButtons.forEach() { $0.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) }
+//            selectedButtons.removeAll()
+//            game.resetCards(at: index)
+//        }
+//
+//        func deselectCard() {
+//            card.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+//            card.layer.borderWidth = 3.0
+//            selectedButtons.remove(at: selectedButtons.firstIndex(of: card)!)
+//            game.deSelectCard(at: index)
+//        }
+//
+//        func selectCard(){
+//            card.layer.borderColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+//            card.layer.borderWidth = 3.0
+//            game.selectCard(at: index)
+//            alertLabel.text = "Sets Found: \(game.setCount)"
+//        }
+//
+//        if selectedButtons.contains(card) {
+//            deselectCard()
+//            return
+//        } else {
+//            selectedButtons.append(card)
+//            selectCard()
+//
+//        }
+//
+//        if selectedButtons.count == 3 {
+//            resetCards()
+//            if game.selectedCardsAreASet {
+//                alertLabel.text = "That's a set! =)"
+//            } else {
+//                alertLabel.text = "Not a set! =("
+//            }
+//
+//        }
+//
+//    }
+//
+//    private func addGameCards(){
+//        if activeCardButtons < 23 {
+//            activeCardButtons += 3
+//            updateView()
+//        }
+//    }
+//
+//    private func removeCardButtons() {
+//        activeCardButtons -= 3
+//        updateView()
+//    }
+//
+//    //MARK: UI Setup
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        setUpGame()
+//    }
+//
+//    private func setUpGame() {
+//        game = Game()
+//        activeCardButtons = 11
+//        for button in setCardButtons {
+//            button.isHidden = true
+//        }
+//        updateView()
+//    }
+//
+//    private func updateView() {
+//        for index in 0...activeCardButtons{
+//            print(game.cardsOnTheTable.count)
+//            let button = setCardButtons[index]
+//            let card = game.cardsOnTheTable[index]
+//            button.setAttributedTitle(setCardTitle(with: card), for: .normal)
+//            button.isHidden = false
+//        }
+//
+//        scoreLabel.text = "Score: \(game.score)"
+//        deckCountLabel.text = "Deck Count: \(game.setCardDeck.count)"
+//    }
+//
+//    private func setCardTitle(with card: Card) -> NSAttributedString {
+//        var cardTitle = String()
+//        if let shape = ModelToView.shapes[card.cardShape] {
+//            cardTitle = shape
+//            switch card.cardNumber {
+//            case .two: cardTitle.append(" \(shape)")
+//            case .three: cardTitle.append(" \(shape) \(shape)")
+//            default:
+//                break
+//            }
+//        }
+//        let attributes: [NSAttributedString.Key : Any] = [
+//            .strokeColor: ModelToView.colors[card.cardColor]!,
+//            .strokeWidth: ModelToView.strokeWidth[card.cardFill]!,
+//            .foregroundColor: ModelToView.colors[card.cardColor]!.withAlphaComponent(ModelToView.alpha[card.cardFill]!),
+//        ]
+//        return NSAttributedString(string: cardTitle, attributes: attributes)
+//    }
+//}
